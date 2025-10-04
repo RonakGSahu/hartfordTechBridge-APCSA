bd5cce5f5fd896842f37d3ea55a75b5fc082da8a
Eli Shattuck <eli.shattuck@uconn.edu>
2025-10-03 22:19:59 -0400

    Revert "fixed extra dir"
    This reverts commit 27a0f62cc1f86bd699bd6ccdd5c2ea9a7eabbc39.

---
+++ starter/loop_practice/forLoops/Q3.java
@@ -0,0 +1,25 @@
+
+/**
+ * Question 3.
+ * Implement the {@code countEvens} method. Starter code is provided below.
+ *
+ * This method should return the number of even integers in a given array.
+ * For example, countEvens({1, 2, 4, 5, 6}) should return 3 (2, 4, and 6).
+ * 
+ * Hint: Remember the modulo "%" operator! Even numbers are divisible by 2, 
+ * so x is even if (x % 2) is 0!
+ * 
+ * @author (your name)
+ * @version (today's date)
+ */
+public class Q3 {
+    /**
+     * Returns the number of even integers in the array.
+     *
+     * @param nums the array of integers
+     * @return the count of even numbers
+     */
+    public static int countEvens(int[] nums) {
+        return 0; // replace with your implementation
+    }
+}
---
+++ starter/loop_practice/forLoops/Q4.java
@@ -0,0 +1,24 @@
+
+/**
+ * Question 4.
+ * Implement the {@code contains} method. Starter code is provided below.
+ *
+ * This method should return true if the given array contains the target value.
+ * For example, contains({1, 2, 3, 4}, 3) should return true.
+ * If the value is not found, return false.
+ * 
+ * @author (your name)
+ * @version (today's date)
+ */
+public class Q4 {
+    /**
+     * Checks if the target value exists in the array.
+     *
+     * @param nums the array of integers
+     * @param target the value to search for
+     * @return true if target is in the array, false otherwise
+     */
+    public static boolean contains(int[] nums, int target) {
+        return false; // replace with your implementation
+    }
+}
